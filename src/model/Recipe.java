65e6c15443c4ebadee2ebffb0dae483e3abef983
d42n81 <33154840+d42n81@users.noreply.github.com>
2018-10-07 09:40:10 -0400

    Update Recipe.java
    Added in full recipe.java file

--- src/model/Recipe.java
+++ src/model/Recipe.java
@@ -1,6 +1,23 @@
 package model;
 
-public interface Recipe {
-	String [] ingredientArray = new String [10];
-	//testing
+public class recipe {
+	int size = 0;
+	public Item[] foods = new Item[100];
+	public String name;
+	String link;
+	
+	public recipe(String name, Item[] foods, String url ) {
+		this.link = url;
+		this.name = name;
+		this.foods = foods;
+		
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	public String getLink() {
+		return this.link;
+	}
+
 }
